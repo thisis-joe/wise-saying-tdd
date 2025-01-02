0346df3f124577766f6856e9f75d93112dcde2c0
joseph <wdg0434@gmail.com>
2025-01-02 15:13:20 +0900

    tdd test1

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner("등록\n현재를 사랑하라.\n작자미상.\n");
+        //Scanner scan = new Scanner(System.in);
+
+        String val1 = scan.nextLine();
+        System.out.println(val1);
+
+        String val2 = scan.nextLine();
+        System.out.println(val2);
+
+        String val3 = scan.nextLine();
+        System.out.println(val3);
+
+        //테스트 봇 만들기
+
+    }
+}
---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class FirstTest {
+    @Test
+    void t1(){
+        assertThat(1).isEqualTo(1);
+    }
+
+}
